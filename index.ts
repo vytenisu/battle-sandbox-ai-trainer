ef543e4e32881446aaa19f94c824a19e15dc26ff
Vytenis <wyt.dislike@gmail.com>
2022-11-21 14:21:42 +0200

    Initial skeleton

---
+++ index.ts
@@ -0,0 +1,20 @@
+// import {connectToMapGeneration} from './lib/generator'
+// import {launchController} from './lib/controller'
+// import {ELogLevel, info, init} from './lib/utils/log'
+// import packageInfo from './package.json'
+// import {launchInterface} from './lib/interface'
+
+// init('Controller', ELogLevel.verbose)
+
+// info(`${packageInfo.name} ${packageInfo.version}`)
+// info(`by ${packageInfo.author.name}`)
+// ;(async () => {
+//   await connectToMapGeneration()
+//   launchController()
+//   launchInterface()
+//   // setInterval(() => {
+//   //   getNewMap()
+//   // }, 7000)
+// })()
+
+// // TODO: NOT FINISHED

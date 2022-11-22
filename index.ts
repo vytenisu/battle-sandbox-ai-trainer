18c6d8f484601d811f8b777c1f6c7c170d744a49
Vytenis <wyt.dislike@gmail.com>
2022-11-22 17:41:22 +0200

    Cleanup

--- index.ts
+++ index.ts
@@ -1,13 +1,7 @@
-import {normalizeSample} from './lib/utils/normalizer'
-import {runBattle} from './lib/utils/runner'
 import './lib/constants/global'
-import {connectToController, resetMap} from './lib/services/controller'
+import {connectToController} from './lib/services/controller'
 import {ELogLevel, info, init} from './lib/utils/log'
 import packageInfo from './package.json'
-import {BaselineBot} from './lib/utils/baseline-bot'
-import './lib/utils/network' // For code suggestions only
-import {Network} from './lib/utils/network'
-import {generateSamples} from './lib/utils/sample-generator'
 import {ETrainingStrategy, trainNetwork} from './lib/utils/trainer'
 import {MODEL_PATH} from './lib/constants/config'
 
